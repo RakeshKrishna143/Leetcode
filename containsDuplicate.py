af1af4855599c0e867718ee76faf010b1b875a33
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-07-06 20:39:09 +0530

    Create containsDuplicate.py

---
+++ containsDuplicate.py
@@ -0,0 +1,19 @@
+
+def containsDuplicate(a):
+    def maxfun(a):
+        m=a[0]
+        for i in range(1,len(a)):
+            if a[i]>m:
+                m=a[i]
+        return m
+    m=maxfun(a)
+    b=[0]*(m+1)
+    for i in a:
+        b[i]+=1
+    for i in b:
+        if i>1:
+            return True
+    return False
+            
+a=[1,2,1]
+print(containsDuplicate(a))
