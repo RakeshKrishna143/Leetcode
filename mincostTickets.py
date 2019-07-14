f8e1b8b6ce7b5d378b2cccbed9b0707d09add2ce
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-07-15 00:27:43 +0530

    Update mincostTickets.py

--- mincostTickets.py
+++ mincostTickets.py
@@ -1,24 +1,21 @@
-
-def mincostTickets(days,costs):
-    dp = [0]*366
-    j = 0
-    for i in range(days[0], 366):
-        if i == days[j]:
-            dp[i] = dp[i-1]+costs[0]
-            if i >= 7:
-                dp[i] = min(dp[i],dp[i-7]+costs[1])
+class Solution:
+    def mincostTickets(self, days: 'List[int]', costs: 'List[int]') -> 'int':
+        dp = [0]*(max(days)+1)
+        j = 0
+        for i in range(days[0], (max(days)+1)):
+            if i == days[j]:
+                dp[i] = dp[i-1]+costs[0]
+                if i >= 7:
+                    dp[i] = min(dp[i],dp[i-7]+costs[1])
+                else:
+                    dp[i] = min(dp[i], costs[1])
+                if i >= 30:
+                    dp[i] = min(dp[i],dp[i-30]+costs[2])
+                else:
+                    dp[i] = min(dp[i], costs[2])
+                j += 1
+                if j == len(days):
+                    return dp[-1]
             else:
-                dp[i] = min(dp[i], costs[1])
-            if i >= 30:
-                dp[i] = min(dp[i],dp[i-30]+costs[2])
-            else:
-                dp[i] = min(dp[i], costs[2])
-            j += 1
-            if j == len(days):
-                return dp[i]
-        else:
-            if i > 0:
-                dp[i] = dp[i-1]
-days = [1,4,6,7,8,20]
-costs = [2,7,15]                
-print(mincostTickets(days,costs))
+                if i > 0:
+                    dp[i] = dp[i-1]
